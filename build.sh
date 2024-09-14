16d92623dd6abd9ed827d9d286a64dca4e36d4ff
Joey Murphy <tjmurphy@mit.edu>
2024-09-14 09:16:12 -0400

    rename mas asm

--- build.sh
+++ build.sh
@@ -3,9 +3,9 @@ mipsel-linux-gnu-cpp -Iinclude -undef -Wall -fno-builtin -Dmips -D__GNUC__=2 -D_
 
 cat preproc.c | ./cc1-gcc2.8.1-psx -G0 -w -O2 -quiet -mcpu=3000 -fgnu-linker -mgas -gcoff > gcc_asm.s
 
-cat gcc_asm.s | python3 maspsx/maspsx.py > menos_asm.s
+cat gcc_asm.s | python3 maspsx/maspsx.py > mas_asm.s
 
-cat menos_asm.s | mipsel-linux-gnu-as -G0 -O2 -march=r3000 -mtune=r3000 -o compiled.o
+cat mas_asm.s | mipsel-linux-gnu-as -G0 -O2 -march=r3000 -mtune=r3000 -o compiled.o
 
 mips-linux-gnu-objdump --disassemble compiled.o
 
